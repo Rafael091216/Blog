ea038898d6bb4da36cf166a35cbb70d95bb83462
Sistemas03 <rafita_1320@hotmail.com>
2020-02-12 12:24:07 -0600

    test

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,30 @@ namespace Blog
     {
         static void Main(string[] args)
         {
+
+
+           // //Creating Blog object
+           //Blogs bsObj = new Blogs()
+           //{
+           //    Name = "estradawebgroup",
+           //    Description = "Compartir conocimiento"
+           //};
+           // //Convert Blog object to JOSN string format
+           // string jsonData = JsonConvert.SerializeObject(bsObj);
+           // Console.Write(jsonData);
+           // Console.ReadKey();
+
+            string json = @"{
+'Unidad': '907535223st',
+'Descripcion': 'UB 1525',
+}";
+
+            Blogs m = JsonConvert.DeserializeObject<Blogs>(json);
+            Console.Write(m.Unidad);
+            Console.WriteLine(m.Descripcion);
+            Console.ReadKey();
+
+
         }
     }
 }
